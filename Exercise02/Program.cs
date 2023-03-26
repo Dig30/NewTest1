69637445a64023b743af24ba7334415e7402f3e0
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-03-26 19:36:45 +0300

    второе задание

---
+++ Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задание 2//
+Console.Write("Введите число а: ");
+string namber1 = Console.ReadLine();
+
+Console.Write("Введите число b: ");
+string namber2 = Console.ReadLine();
+int a = int.Parse(namber1);
+int b = int.Parse(namber2);
+
+if (a == b * b) {
+    Console.WriteLine("yes");
+} else {
+    Console.WriteLine("no");
+}
